76987ec0103037728df832954296769b1b79db83
Clifford Musyoka <Clifford Musyoka>
2024-05-17 15:08:02 +0300

    Make mobile Responsive

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
 const modal = document.getElementById('modal');
 const openModal = document.getElementById('openModal');
  const closeIcon = document.getElementsByClassName("close-icon")[0];
+ const thankYouPage = document.getElementById("thankYouPage");
+ const closeThankYouBtn = document.getElementById("closeThankYouBtn");
+const openThankYouBtn = document.getElementById("openThankYouBtn");
+const pledge = document.getElementsByClassName('pledge');
 
 openModal.onclick = function() {
     modal.style.display = "block";
+    document.body.classList.add('fixed');
   }
 
   closeIcon.onclick = function() {
     modal.style.display = "none";
+    document.body.classList.remove('fixed');
   }
+
+  openThankYouBtn.onclick = function(){
+    modal.style.display = "none";
+    thankYouPage.style.display = "block"
+  }
+
+  closeThankYouBtn.onclick = function(){
+    thankYouPage.style.display = "none";
+    document.body.classList.remove('fixed');
+  }
+
+ 
